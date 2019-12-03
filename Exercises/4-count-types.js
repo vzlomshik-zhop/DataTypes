5c871cd32bc76d0ea1c2cb62377b0a10742f4730
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2019-12-03 16:52:12 +0200

    Update 4-count-types.js

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,14 @@
 'use strict';
 
 const countTypesInArray = null;
+let b = {};
+for(let element of countTypesInArray) {
+  let t = typeof(element);
+  if (b[t] === undefined)
+    b[t] = 1;
+  else
+    b[t]++;
+}
+console.log(b);
 
 module.exports = { countTypesInArray };
